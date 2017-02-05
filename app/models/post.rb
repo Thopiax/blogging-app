be989f30e88221f22197697aade19b7bfdde0851
Norbert Podsadowski <norbo11@googlemail.com>
2017-02-05 08:25:37 +0000

    Default values

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 # Model for the posts in the blogging service
 class Post < ActiveRecord::Base
   validates :message, presence: true, length: { maximum: 9999999 }
+  attr_accessor :sentiment_colour
+  attr_accessor :emojis
   has_many :keywords
+
+  after_initialize :init
+
+  def init
+    self.sentiment_colour  ||= '#ffffff'           #will set the default value only if it's nil
+    self.emojis            ||= '' #let's you set a default association
+  end
 end
